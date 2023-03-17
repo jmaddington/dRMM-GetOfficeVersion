2e5dcde4d92a80ab7ea4bb38f55f4ee5ba3ed293
Jonathan Addington <jonathan.addington@jmaddington.com>
2023-03-17 16:54:52 -0400

    Renamed output component name

--- build/repackage.bat
+++ build/repackage.bat
@@ -5,6 +5,6 @@ cd ..
 copy build\resource.xml
 copy command.ps1 command.bat
 del aem-component.cpt
-.\bin\7zip\7z.exe a  aem-component.cpt -tzip * -x!.git/ -x!initialize/ -x!build/
+.\bin\7zip\7z.exe a  dRMM-component.cpt -tzip * -x!.git/ -x!initialize/ -x!build/
 del resource.xml
 del command.bat
